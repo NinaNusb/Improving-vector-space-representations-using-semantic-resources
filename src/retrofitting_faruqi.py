c79a43d3c34dfc5b602e8218a0ff8507164f332b
Lulu Lemon <nina.nusbaumer@gmail.com>
2023-04-16 11:57:32 +0200

    implementation faruqui algo

--- src/retrofitting_faruqi.py
+++ src/retrofitting_faruqi.py
@@ -8,39 +8,65 @@ import sys
 from copy import deepcopy
 
 isNumber = re.compile(r'\d+.*')
+
 def norm_word(word):
+  """
+  - input: word
+  - return: a normalized version of it
+  Normalization process: includes checking if the word is a number or a punctuation mark and replacing it with special tokens
+  """
   if isNumber.search(word.lower()):
     return '---num---'
+  # check if the word consists only of non-alphanumeric characters by removing all non-alphanumeric characters from the word 
+  # and checking if the result is an empty string
   elif re.sub(r'\W+', '', word) == '':
     return '---punc---'
   else:
+  # if input word not a number nor a punctuation mark, return a lowercase version of input word
     return word.lower()
 
 ''' Read all the word vectors and normalize them '''
 def read_word_vecs(filename):
+  """
+  - input: name of the file containing the word vectors
+  """
   wordVectors = {}
   if filename.endswith('.gz'): fileObject = gzip.open(filename, 'r')
   else: fileObject = open(filename, 'r')
   
   for line in fileObject:
     line = line.strip().lower()
+    # The first word is assumed to be the word itself, and the remaining words are assumed to be the components of the word vector
     word = line.split()[0]
+    # initialize a numpy array of zeros with the same length as the word vector
     wordVectors[word] = numpy.zeros(len(line.split())-1, dtype=float)
     for index, vecVal in enumerate(line.split()[1:]):
+      # assign the values in the numpy array to the corresponding components of the word vector
       wordVectors[word][index] = float(vecVal)
     ''' normalize weight vector '''
+    # divide each element by the square root of the sum of the squares of all the elements in the array
+    # plus a small constant (1e-6) to avoid division by zero
     wordVectors[word] /= math.sqrt((wordVectors[word]**2).sum() + 1e-6)
-    
+  
+  # standard error indicating that the vectors have been read from the file 
   sys.stderr.write("Vectors read from: "+filename+" \n")
   return wordVectors
 
 ''' Write word vectors to file '''
 def print_word_vecs(wordVectors, outFileName):
+  """
+  - input: a dictionary wordVectors where keys are words and values are their corresponding word vectors
+           file name outFileName
+  """
   sys.stderr.write('\nWriting down the vectors in '+outFileName+'\n')
   outFile = open(outFileName, 'w')  
   for word, values in wordVectors.iteritems():
     outFile.write(word+' ')
     for val in wordVectors[word]:
+      # write the word vectors to the ouptu file in the format:
+      # word1 val1 val2 val3 ...
+      # word2 val1 val2 val3 ...
+      # ...
       outFile.write('%.4f' %(val)+' ')
     outFile.write('\n')      
   outFile.close()
@@ -77,15 +103,24 @@ def retrofit(wordVecs, lexicon, numIters):
 if __name__=='__main__':
 
   parser = argparse.ArgumentParser()
+  # input: The file path to the input word vectors file
   parser.add_argument("-i", "--input", type=str, default=None, help="Input word vecs")
+  # lexicon: The file path to the lexicon file
   parser.add_argument("-l", "--lexicon", type=str, default=None, help="Lexicon file name")
+  # output: The file path to the output word vectors file
   parser.add_argument("-o", "--output", type=str, help="Output word vecs")
+  # numiter: The number of iterations to run the retrofitting algorithm (default is 10)
   parser.add_argument("-n", "--numiter", type=int, default=10, help="Num iterations")
   args = parser.parse_args()
 
-  wordVecs = read_word_vecs(args.input)
+  # reads the input word vectors from the file specified by the -i or --input command line argument using the read_word_vecs function
+  wordVecs = read_word_vecs(args.input)  
+  # reads the lexicon from the file specified by the -l or --lexicon command line argument using the read_lexicon function
   lexicon = read_lexicon(args.lexicon)
+  # sets the number of iterations for retrofitting to the value specified by the -n or --numiter command line argument
   numIter = int(args.numiter)
+  # calls the retrofit function to enrich the word vectors using the lexicon, runs for the specified number of iterations,
+  # and writes the enriched word vectors to the output file specified by the -o or --output command line argument using the print_word_vecs function.
   outFileName = args.output
   
   ''' Enrich the word vectors using ppdb and print the enriched vectors '''
